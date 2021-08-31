6b6c4a030bb3d185ef46cf77e41dfdac2a53879d
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-08-31 12:15:20 -0400

    Testing slash commands

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -17,22 +17,3 @@ module.exports = {
   }
 }
 
-run: async (bot, message, args) => {
-const interactions = require("discord-slash-commands-client");
-
-const client = new interactions.Client(
-  "NzMxNjUwODAyMjk2NDIyNDcw.XwpI6A.qz2a8Gw1ubU5ZVy1sSnGMk081Ys",
-  "731650802296422470"
-);
-
-
-client.createCommand({
-  name: "test",
-  description: "description for this command",
-})
-
-message.channel.send('srnyx is cringe eggsdee')
-
-.catch(console.error)
-.then(console.log);
-}
