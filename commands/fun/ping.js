8f09727a2b17b3c61fe3734b1857ff8a820cd733
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-08-31 12:11:53 -0400

    tweaks

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -17,15 +17,16 @@ module.exports = {
   }
 }
 
+run: async (bot, message, args) => {
 const interactions = require("discord-slash-commands-client");
 
-const bot = new interactions.Client(
+const client = new interactions.Client(
   "NzMxNjUwODAyMjk2NDIyNDcw.XwpI6A.qz2a8Gw1ubU5ZVy1sSnGMk081Ys",
   "731650802296422470"
 );
 
 
-bot.createCommand({
+client.createCommand({
   name: "test",
   description: "description for this command",
 })
@@ -34,3 +35,4 @@ message.channel.send('srnyx is cringe eggsdee')
 
 .catch(console.error)
 .then(console.log);
+}
