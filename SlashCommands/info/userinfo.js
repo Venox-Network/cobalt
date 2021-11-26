b2be3c145e1cb1c9fd736c6a9b97077b647f26f6
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-11-25 22:13:41 -0500

    test

--- SlashCommands/info/userinfo.js
+++ SlashCommands/info/userinfo.js
@@ -1,4 +1,4 @@
-const { Client, CommandInteraction } = require("discord.js");
+const { Client, MessageEmbed, CommandInteraction } = require("discord.js");
 
 module.exports = {
     name: "userinfo",
@@ -11,11 +11,29 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, interaction, args) => {
-        interaction.reply({ 
-            content: "Work in progress :(", 
-            ephemeral: true 
-        
-        
+
+        const user = message.author || client.users.cache.get(args[0]); //|| message.author; //message.guild.members.get()
+        if(!user) return message.channel.send('Couldn\'t find that user :(');
+        const member = message.guild.member(user);
+          const embed = new MessageEmbed()
+            .setColor('0070c0')
+            .setAuthor(user.tag)
+            .setDescription(`${user}`)
+            .setThumbnail(user.displayAvatarURL)
+               .addField('Join Date:', `${member.joinedAt}`, true)
+            .addField("Account Creation Date:", member.user.createdAt, true)
+            .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+            .addField("Roles:", member.roles.cache.map(roles => `${roles}`).join(', '), true)
+            .addField("Custom Status:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+            .addField("Status:", `${user.presence.status}`, true)
+            .addField('User\'s ID:', user.id, true)
+    
+            .setFooter(`© ${message.guild.me.displayName} | Guild ID: ${message.guild.id}`);
+
+
+        interaction.followUp({ 
+            embeds: [embed],
+            ephemeral: true
         }).catch(console.error);
     },
 };
