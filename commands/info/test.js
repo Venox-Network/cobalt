64e2e82a97b0ba584f087a33c4aa1aaba8b6b8f6
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-12-01 01:01:07 -0500

    test

--- commands/info/test.js
+++ commands/info/test.js
@@ -47,7 +47,7 @@ module.exports = {
             label: "fourth option",
             value: "fourth",
             description: "fourth boi",
-          },second
+          },
         ])
     );
 
