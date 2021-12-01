888ed4c20e0dff949d91c2d4e1c20fafc5c5bd17
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-12-01 18:58:19 -0500

    test

--- commands/info/test.js
+++ commands/info/test.js
@@ -30,7 +30,7 @@ module.exports = {
           {
             label: "FAQ #1",
             value:
-              [embed],
+              `${embed}`,
             description: "What is this place?!",
           },
           {
