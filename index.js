ed67095ebace1b582ee3892499570ff44b21dc63
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-12-05 00:06:55 -0500

    pls work

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const Discord = require("discord.js");
 const client = new Client({
     intents: 32767,
 });*/
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_INTEGRATIONS", /*"GUILD_MEMBERS", "GUILD_PRESENCES",*/ "GUILD_WEBHOOKS", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "GUILD_VOICE_STATES"] });
+const client = new Discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_INTEGRATIONS",
+    /*"GUILD_MEMBERS", "GUILD_PRESENCES",*/ "GUILD_WEBHOOKS",
+    "GUILD_MESSAGE_REACTIONS",
+    "GUILD_MESSAGE_TYPING",
+    "GUILD_VOICE_STATES",
+  ],
+});
 module.exports = client;
 
 // Global Variables
@@ -17,26 +27,17 @@ require("./handler")(client);
 
 // Status
 client.on("ready", () => {
-    client.user.setStatus("online");
-  
-    // this event means, that it will do something when the bot is online
-    // i will log in the console that the bot is online when its online
-    console.log(
-      `${client.user.username} boutta' watch ${client.users.cache.size} users and ${client.guilds.cache.size} servers!`
-    );
-    // activity
-  
-    const status = [
-      `${client.guilds.cache.size} servers and ${client.users.cache.size} users!`,
-      'official server: dsc.gg/venoxnet',
-  
-  
-    ]; // type here ur status how to do the variable ${bot.guilds.size}
-    // it can be users too ok  ok
-  
-    setInterval(() => {
-      client.user.setActivity(status.random(), { type: "WATCHING" });
-    }, 10000);
+  client.user.setPresence({
+    status: "online",
+    activity: {
+      name: "sudoku",
+      type: "PLAYING",
+    },
   });
+
+  console.log(
+    `${client.user.username} boutta' watch ${client.users.cache.size} users and ${client.guilds.cache.size} servers!`
+  );
+});
 
 client.login(client.config.token);
