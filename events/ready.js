957923caaf9bf8b2719c1c874ea3eee83514b5e5
srnyx <roweblox@gmail.com>
2021-12-06 10:43:32 -0500

    Change status
    Changed the status for music update

--- events/ready.js
+++ events/ready.js
@@ -9,8 +9,8 @@ bot.on('ready', function() {
     const servers = bot.guilds.cache.size;
     const servercount = bot.guilds.cache.reduce((a,b) => a+b.memberCount, 0);
     // ${servers} servers & ${servercount} users
-    bot.user.setActivity(`v!help | chrizftw.cf`, { type: `LISTENING` }); // PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
-    bot.user.setStatus("online");
+    bot.user.setActivity(`NOW WITH MUSIC! srnyx.xyz/venox`, { type: `LISTENING` }); // PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
+    bot.user.setStatus("idle");
   });
 
 
