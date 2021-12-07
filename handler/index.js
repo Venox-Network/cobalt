236c8891fb6b066904db4f265392103d9e227b2b
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-12-06 23:00:07 -0500

    bruh

--- handler/index.js
+++ handler/index.js
@@ -5,6 +5,7 @@ const { Client } = require("discord.js");
 const express = require('express');
 const app = express();
 const Discord = require("discord.js");
+const {Player} = require('discord-player');
 //const client = new Discord.Client({ ws: { intents: 32509 }));
 /*const { Client, Intents } = require('discord.js');
 const myIntents = new Intents();
@@ -79,3 +80,33 @@ module.exports = async(client) => {
     mongoose.connect(mongooseConnectionString).then(() => console.log('Connected to mongodb'));
 
 };
+
+const player = new Player(client);
+
+player.on('error', (queue, error) => {
+    console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+  });
+  
+  player.on('connectionError', (queue, error) => {
+    console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+  });
+  
+  player.on('trackStart', (queue, track) => {
+    queue.metadata.send(`▶ | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+  });
+  
+  player.on('trackAdd', (queue, track) => {
+    queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+  });
+  
+  player.on('botDisconnect', queue => {
+    queue.metadata.send('❌ | I was manually disconnected from the voice channel, clearing queue!');
+  });
+  
+  player.on('channelEmpty', queue => {
+    queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+  });
+  
+  player.on('queueEnd', queue => {
+    queue.metadata.send('✅ | Queue finished!');
+  });
