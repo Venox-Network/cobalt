0b2f5f40115cbf12c596b30d5f6600b8951de27e
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2021-12-06 19:54:31 -0500

    bruh

--- SlashCommands/music/leave.js
+++ SlashCommands/music/leave.js
@@ -12,9 +12,9 @@ module.exports = {
   run: async (client, interaction, args) => {
     if (interaction.guild.me.voiceChannel !== undefined) {
       interaction.guild.me.voiceChannel.leave();
-      interaction.reply({content: "Disconnected ✅", ephemeral:true});
+      interaction.followUp({content: "Disconnected ✅"});
     } else {
-      interaction.reply("I'm not connected to a voice channel!");
+      interaction.followUp({content: "I'm not connected to a voice channel.."});
     }
   },
 };
