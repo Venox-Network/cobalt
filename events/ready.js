3ed58a2e10f1df1b33f0821ef5e89b010d91fd29
srnyx <roweblox@gmail.com>
2021-12-11 12:48:55 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ bot.on('ready', function() {
     const servers = bot.guilds.cache.size;
     const servercount = bot.guilds.cache.reduce((a,b) => a+b.memberCount, 0);
     // ${servers} servers & ${servercount} users
-    bot.user.setActivity(`/play song | srnyx.xyz/venox`, { type: `LISTENING` }); // PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
+    bot.user.setActivity(`the future: v.srnyx.xyz`, { type: `WATCHING` }); // PLAYING, WATCHING, LISTENING, STREAMING, COMPETING
     bot.user.setStatus("idle");
   });
 
