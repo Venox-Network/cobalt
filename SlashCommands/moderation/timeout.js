3b67766cb82dc9d33c5684dff069365fb85a47b0
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-02 20:08:27 -0500

    fix

--- SlashCommands/moderation/timeout.js
+++ SlashCommands/moderation/timeout.js
@@ -1,49 +1,41 @@
-const { Command } = require("reconlx"); 
+const { Command } = require("reconlx");
 const ms = require("ms");
 module.exports = new Command({
-    name: 'timemout',
-    description: 'timemout a member',
-    aliases: ['mute'],
-    options: [
-        {
-            name: 'user',
-            description: 'member to peform the timemout',
-            type: 'USER',
-            required: true
-        },
-        {
-            name: 'length',
-            description: 'length of the timemout',
-            type: 'STRING',
-            required: true
-        },
-        {
-            name: 'reason',
-            description: 'reason for the timemout',
-            type: 'STRING',
-            required: true
-        }
-    ],
-    /**
-   *
-   * @param {Client} client
-   * @param {Message} message
-   * @param {String[]} args
-   * 
-   * (client, interaction, args)
-   */
-    run: async({interaction}) => {
-        const user = interaction.options.getUser('user')
-        const length = interaction.options.getString('length')
-        const reason = interaction.options.getString('reason')
-        const members = interaction.guild.members.cache.get(user.id)
-        
-        const timeInMs = ms(length);
-        if (!timeInMs)
-            return interaction.followUp('Please specify a valid time!');
+  name: "timemout",
+  description: "timemout a member",
+  aliases: ["mute"],
+  options: [
+    {
+      name: "user",
+      description: "member to peform the timemout",
+      type: "USER",
+      required: true,
+    },
+    {
+      name: "length",
+      description: "length of the timemout",
+      type: "STRING",
+      required: true,
+    },
+    {
+      name: "reason",
+      description: "reason for the timemout",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: async ({ interaction }) => {
+    const user = interaction.options.getUser("user");
+    const length = interaction.options.getString("length");
+    const reason = interaction.options.getString("reason");
+    const members = interaction.guild.members.cache.get(user.id);
 
-        member.timeout(timeInMs, reason);
-        interaction.followUp(`${user} has been muted for ${length}.\nReason: ${reason}`);
+    const timeInMs = ms(length);
+    if (!timeInMs) return interaction.followUp("Please specify a valid time!");
 
-    }
+    member.timeout(timeInMs, reason);
+    interaction.followUp(
+      `${user} has been muted for ${length}.\nReason: ${reason}`
+    );
+  },
 });
