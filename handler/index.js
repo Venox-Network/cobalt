fc762ed9979a36cefbd228fb5bb38dc8e7bbf02f
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-06 17:52:34 -0500

    ok uh pls don't break :((

--- handler/index.js
+++ handler/index.js
@@ -52,7 +52,7 @@ module.exports = async(client) => {
 
     // Slash Commands
     const slashCommands = await globPromise(
-        `${process.cwd()}/SlashCommands/*/*.js`
+        `${process.cwd()}/slashCommands/*/*.js`
     );
 
     const arrayOfSlashCommands = [];
