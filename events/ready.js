3377498e2afa1d124367c65809db2e8c5fef7920
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-09 15:59:56 -0500

    Activity Status

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,31 @@ const bot = require("../index");
 const config = require("../config.json");
 
 bot.on("ready", () =>
-    console.log(`${bot.user.tag} is up and ready to go! Also I like cookies :)`)
+  console.log(`${bot.user.tag} is up and ready to go! Also I like cookies :)`)
 );
 
+const servers = bot.guilds.cache.size;
+const servercount = bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
+
+const activities = [
+  "the future.",
+  "srnyx's Modpack: srnyx.xyz",
+  "Chriz's Graphics: chrizftw.cf",
+  `${servers} servers & ${servercount} users`,
+];
+
+bot.on("ready", () => {
+  // run every 10 seconds
+  setInterval(() => {
+    // generate random number between 1 and list length.
+    const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
+    const newActivity = activities[randomIndex];
+    bot.user.setActivity(newActivity, { type: `WATCHING` });
+    bot.user.setStatus("online");
+  }, 10000);
+});
+
+/*
 bot.on('ready', function() {
     const servers = bot.guilds.cache.size;
     const servercount = bot.guilds.cache.reduce((a,b) => a+b.memberCount, 0);
@@ -14,7 +36,7 @@ bot.on('ready', function() {
   });
 
  
-
+*/
 /*
 bot.on("ready", async() => {
     
