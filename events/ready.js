b4235c6a4ed15393a4fb16fcabb76c306f54d3f8
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-09 19:32:32 -0500

    Installed Dependabot + New Bot Presence

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const config = require("../config.json");
 bot.on("ready", () =>
   console.log(`${bot.user.tag} is up and ready to go! Also I like cookies :)`)
 );
-
+/*
 const servers = bot.guilds.cache.size;
 const servercount = bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
 
@@ -15,6 +15,7 @@ const activities = [
   `${servers} servers & ${servercount} users`,
 ];
 
+
 bot.on("ready", () => {
   // run every 10 seconds
   setInterval(() => {
@@ -24,6 +25,24 @@ bot.on("ready", () => {
     bot.user.setActivity(newActivity, { type: `WATCHING` });
     bot.user.setStatus("online");
   }, 10000);
+});
+*/
+
+const state = 0;
+const presences = [
+    { type: 'PLAYING',  message: 'a game'  },
+    { type: 'WATCHING', message: 'a video' },
+    { type: 'LISTENING', message: 'a song' },
+];
+
+bot.on("ready", () => {
+
+setInterval(() => {
+    state = (state + 1) % presences.length;
+    const presence = presences[state];
+
+    client.user.setActivity(presence.message, { type: presence.type });
+}, 10000);
 });
 
 /*
@@ -37,32 +56,3 @@ bot.on('ready', function() {
 
  
 */
-/*
-bot.on("ready", async() => {
-    
-    const servers = await bot.guilds.cache.size;
-    const servercount = await bot.guilds.cache.reduce((a,b) => a+b.memberCount, 0);
-
-    
-    bot.user.setPresence({
-        status: 'dnd',
-        activity: {
-            name: 'a video',
-            type: 'WATCHING'
-         }
-      });
-    
-    
-    const messages = [
-        `Owned by srnyx & ChrizxzFTW`,
-        `Join the network: dsc.gg/venoxnet`,
-        `Watching ${servers} servers and ${servercount} members!`
-    ]
-
-    setInterval(() => {
-        const status = messages[Math.floor(Math.random()*messages.length)]
-        bot.user.setPresence({ messages : [{name : `${status}`}]})
-    }, 5000);
-    
-  });
-  */
