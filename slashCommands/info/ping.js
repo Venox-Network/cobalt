291a32ada19f8e849084eacf96fab9cf94716110
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-11 19:38:24 -0500

    Update Commands
    - Updated ping command
    - Fixed activity commad

--- slashCommands/info/ping.js
+++ slashCommands/info/ping.js
@@ -1,23 +1,26 @@
-const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { Client, CommandInteraction } = require("discord.js");
 
 module.exports = {
-    name: "ping",
-    description: "returns websocket ping",
-    type: 'CHAT_INPUT',
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async(client, interaction, args) => {
-        interaction.followUp("Pinging...");
-        const reply = await interaction.fetchReply();
-		const embed = new MessageEmbed()
-			.setTitle("Pong!")
-			.addField("❤️ Heartbeat", `${client.ws.ping}ms`)
-			.addField("🔁 Roundtrip", `${reply.createdTimestamp - interaction.createdTimestamp}ms`)
-		        .setFooter("Venox, https://us-east-1.tixte.net/uploads/img.srnyx.xyz/circle.png");
-		reply.edit({ content: " ", embeds: [embed] });
-    },
+  name: "ping",
+  description: "returns websocket ping",
+  type: "CHAT_INPUT",
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    interaction.followUp("Pinging...");
+    const reply = await interaction.fetchReply();
+    const embed = new MessageEmbed()
+      .setTitle("Pong!")
+      .setColor("0070c0")
+      .addField("💓 Heartbeat", `${client.ws.ping}ms`)
+      .addField(
+        "🔁 Roundtrip",
+        `${reply.createdTimestamp - interaction.createdTimestamp}ms`
+      );
+    reply.edit({ content: " ", embeds: [embed] });
+  },
 };
