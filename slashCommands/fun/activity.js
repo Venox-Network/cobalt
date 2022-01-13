2e2d7bb6dcc96d21f5c38ddb4e76993f20773c57
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-12 22:12:52 -0500

    Updated Command
    - Updated the activity command

--- slashCommands/fun/activity.js
+++ slashCommands/fun/activity.js
@@ -86,7 +86,7 @@ module.exports = {
     //const [channelID] = args[1];
     const channelID = interaction.options.getChannel("channel").id;
     //const channel = interaction.guild.channels.cache.get(channelID);
-
+    const name = interaction.options.get("activity").name;
     /*
     if (channel.type !== "GUILD_VOICE")
       return interaction.followUp({
@@ -98,51 +98,51 @@ module.exports = {
     if (value === "watchtogether") {
       discordTogether
         .createTogetherCode(channelID, "youtube")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "doodlecrew") {
       discordTogether
         .createTogetherCode(channelID, "doodlecrew")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "fishington") {
       discordTogether
         .createTogetherCode(channelID, "fishing")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "poker") {
       discordTogether
         .createTogetherCode(channelID, "poker")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "chess") {
       discordTogether
         .createTogetherCode(channelID, "chess")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "checkers") {
       discordTogether
         .createTogetherCode(channelID, "checkers")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "betrayal") {
       discordTogether
         .createTogetherCode(channelID, "betrayal")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "lettertile") {
       discordTogether
         .createTogetherCode(channelID, "lettertile")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "wordsnack") {
       discordTogether
         .createTogetherCode(channelID, "wordsnack")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "spellcast") {
       discordTogether
         .createTogetherCode(channelID, "spellcast")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "awkword") {
       discordTogether
         .createTogetherCode(channelID, "awkword")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     } else if (value === "puttparty") {
       discordTogether
         .createTogetherCode(channelID, "puttparty")
-        .then((x) => interaction.followUp(x.code));
+        .then((x) => interaction.followUp(`[Click to start ${name} in <#${channelID}>!](${x.code})`));
     }
 
     /*
