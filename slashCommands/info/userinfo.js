c793b56ffc0bc679806f7b4657e3242b9ed0c4b2
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-13 19:46:39 -0500

    Updated App
    - Updated user info app

--- slashCommands/info/userinfo.js
+++ slashCommands/info/userinfo.js
@@ -17,15 +17,16 @@ module.exports = {
             .setColor('0070c0')
             .setAuthor(target.user.tag)
             .setDescription(`${target.user}`)
-            .setThumbnail(target.user.AvatarURL)
+            .setThumbnail(target.user.AvatarURL({dynamic: true, size: 512}))
             .addField('Member Since:', `<t:${parseInt(target.joinedTimestamp / 1000)}:R>`, true)
             .addField("Discord User Since:", `<t:${parseInt(target.user.createdTimestamp / 1000)}:R>`, true)
             .addField("Nickname:", `${target.user.nickname !== null ? `${target.user.nickname}` : 'Null'}`, true)
             .addField("Roles:", target.roles.cache.map(r => r).join(" ").replace('@everyone', ' ') || "Null", true)
             //.addField("Custom Status:", `${target.user.presence.game ? target.user.presence.game.name : 'Null'}`, true)
             //.addField("Status:", `${target.user.presence.status ? target.user.presence.status.name : 'Null'}`, true)
+            .addField("Guild", `${interaction.guild.name} (\`\`${interaction.guild.id}\`\`)`)
             .addField('User\'s ID:', `${target.user.id}`, true)
-            .setFooter(`Venox Network | Guild ID: ${interaction.guild.id}`, `https://us-east-1.tixte.net/uploads/img.srnyx.xyz/circle.png`);
+            .setFooter(`Venox Network`, `https://us-east-1.tixte.net/uploads/img.srnyx.xyz/circle.png`);
 
     interaction.followUp({ embeds: [embed], ephemeral: true});
   },
