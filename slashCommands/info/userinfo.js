f0342cd3fef6875bad7b153adddfb6d2a8fafe5a
Chrizxz <55815776+Chrizxz@users.noreply.github.com>
2022-01-16 22:24:18 -0500

    [Update Command] User info App has mutual servers

--- slashCommands/info/userinfo.js
+++ slashCommands/info/userinfo.js
@@ -21,10 +21,10 @@ module.exports = {
             .addField('Member Since:', `<t:${parseInt(target.joinedTimestamp / 1000)}:R>`, true)
             .addField("Discord User Since:", `<t:${parseInt(target.user.createdTimestamp / 1000)}:R>`, true)
             //.addField("Nickname:", `${target.user.nickname !== null ? `${target.user.nickname}` : 'Null'}`, true)
-            //.addField("Roles:", target.roles.cache.map(r => r).join(" ").replace('@everyone', ' ') || "Null", true)
+            .addField("Roles:", target.roles.cache.map(r => r).join(" ").replace('@everyone', ' ') || "Null", true)
             //.addField("Custom Status:", `${target.user.presence.game ? target.user.presence.game.name : 'Null'}`, true)
             //.addField("Status:", `${target.user.presence.status ? target.user.presence.status.name : 'Null'}`, true)
-            .addField("Guild", `${interaction.guild.name} (\`\`${interaction.guild.id}\`\`)`, true)
+            .addField("Mutual Servers", client.guilds.cache.filter((u) => u.members.cache.get(interaction.user.id)).map((g) => g.name).join(", "), true)
             .addField('User\'s ID:', `${target.user.id}`, true)
             .setFooter(`Venox Network`, `https://us-east-1.tixte.net/uploads/img.srnyx.xyz/circle.png`);
 
