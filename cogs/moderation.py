b30b09ee7dd36e22e995091ce981e5e23b7dc32d
DWAA1660 <matnip12345@gmail.com>
2022-04-26 14:35:00 -0600

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -18,15 +18,16 @@ cluster_local = CLUSTER
 
 cluster = motor.motor_asyncio.AsyncIOMotorClient(cluster_local)
 db = cluster["VenoxDB"]
-db.createCollection("report_channels")
 collection = db["report_channels"]
-db.createCollection("warns")
 warn_collection = db["warns"]
 global_report_channel = Global_Report_Channel
 # global log channel
 channel_id = Global_Log_Channel
-
-
+try:
+    db.createCollection("report_channels")
+    db.createCollection("warns")
+except:
+    pass
 class moderation(commands.Cog):
 
     def __init__(self, client):
