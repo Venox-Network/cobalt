4285e749db49b7ca3ef383930947139b27b7a7a0
srnyx <25808801+srnyx@users.noreply.github.com>
2022-05-01 23:51:39 -0400

    Update leaveserver.js

--- slashCommands/utilities/leaveserver.js
+++ slashCommands/utilities/leaveserver.js
@@ -4,7 +4,6 @@ const owners = ["273538684526264320", "242385234992037888"];
 module.exports = {
   name: "leaveserver",
   description: "Leaves the server",
-  type: "CHAT_INPUT",
   run: async (client, interaction, args) => {
     if (!owners.includes(interaction.user.id))
       return interaction.followUp("Only the bot owner(s) can use this command.");
