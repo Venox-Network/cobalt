a3cbd5865fd3b8e9ade6bcb96b1e91005cac0f9a
DWAA1660 <matnip12345@gmail.com>
2022-05-18 08:21:47 -0600

    made it embed and title configurable

--- cogs/stickymessage.py
+++ cogs/stickymessage.py
@@ -1,4 +1,5 @@
 import motor.motor_asyncio
+import nextcord
 from nextcord import Interaction, slash_command
 from nextcord.ext import commands, application_checks
 
@@ -20,27 +21,30 @@ class stickymessage(commands.Cog):
 
     @application_checks.has_permissions(manage_messages=True)
     @slash_command(name="stickysetup", description="Creates a sticky message")
-    async def stickysetup(self, interaction: Interaction, channel_db_id, *, stickymessage, on_or_off: bool):
-        if on_or_off == True:
-            await interaction.response.defer()
-            data = {"_id": channel_id, "guild_id": interaction.guild.id, "guildname": interaction.guild.name,
-                    "channel_db_id": int(channel_db_id), "stickymessage": stickymessage}
-            try:
-                await sticky_messages.insert_one(data)
-                await interaction.send("Successfully added the channel to the database")
-            except:
-                getting_replaced = await sticky_messages.find_one({"_id": channel_db_id})
-                await sticky_messages.replace_one(getting_replaced, data)
-                await interaction.send("Successfully updated the channel in the database")
-        if on_or_off == False:
-            await interaction.response.defer()
-            data = {"_id": channel_id, "guild_id": interaction.guild.id, "guildname": interaction.guild.name,
-                    "channel_db_id": int(channel_db_id), "stickymessage": stickymessage}
-            try:
-                await sticky_messages.delete_one(data)
-                await interaction.send("Successfully removed the channel from the database")
-            except:
-                await interaction.send("Channel not found in the database")
+    async def stickysetup(self, interaction: Interaction, channel_db_id, *, stickymessage, stickytitle="Sticky title"):
+
+        await interaction.response.defer()
+        data = {"_id": channel_id, "guild_id": interaction.guild.id, "guildname": interaction.guild.name,
+                "channel_db_id": int(channel_db_id), "stickymessage": stickymessage, "stickytitle": stickytitle}
+        try:
+            await sticky_messages.insert_one(data)
+            await interaction.send("Successfully added the channel to the database")
+        except:
+            getting_replaced = await sticky_messages.find_one({"_id": channel_db_id})
+            await sticky_messages.replace_one(getting_replaced, data)
+            await interaction.send("Successfully updated the channel in the database")
+
+    @slash_command(name="stickyremove", description="Removes a sticky message")
+    @application_checks.has_permissions(manage_messages=True)
+    async def stickyremove(self, interaction: Interaction, channel_db_id):
+        await interaction.response.defer()
+        data = {"_id": channel_id, "guild_id": interaction.guild.id, "guildname": interaction.guild.name,
+                "channel_db_id": int(channel_db_id)}
+        try:
+            await sticky_messages.delete_one(data)
+            await interaction.send("Successfully removed the channel from the database")
+        except:
+            await interaction.send("Channel not found in the database")
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -51,9 +55,10 @@ class stickymessage(commands.Cog):
         results = await sticky_messages.count_documents({"channel_db_id": message.channel.id})
         if results == 1:
             # check if the message was sent by the bot
-            await message.channel.purge(limit=20, check=lambda message: message.author.id == self.client.user.id)
+            await message.channel.purge(limit=10, check=lambda message: message.author.id == self.client.user.id)
             resultsfind = await sticky_messages.find_one({"channel_db_id": message.channel.id})
-            await message.channel.send(resultsfind["stickymessage"])
+            em = nextcord.Embed(title=resultsfind["stickytitle"], color=0x0561EE, description=resultsfind["stickymessage"])
+            await message.channel.send(embed=em)
         else:
             return
 
