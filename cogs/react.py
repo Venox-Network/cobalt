af94f1d431fd73e9f30d81c9ee730cee74375968
DWAA1660 <matnip12345@gmail.com>
2022-05-17 19:53:43 -0600

    made react command

---
+++ cogs/react.py
@@ -0,0 +1,26 @@
+import asyncio
+import nextcord
+from nextcord import Interaction, slash_command
+from nextcord.ext import commands, application_checks
+
+
+class react(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @application_checks.has_permissions(manage_messages=True)
+    @slash_command()
+    async def react(self, interaction: Interaction, emojis, message_id):
+        await interaction.response.defer()
+        channel = interaction.channel
+        msg = await channel.fetch_message(message_id)
+        emojilist = emojis.split(' ')
+        for emoji in emojilist:
+            await msg.add_reaction(emoji)
+            await asyncio.sleep(1)
+        await interaction.send("added emojis")
+
+
+def setup(client):
+    client.add_cog(react(client))
