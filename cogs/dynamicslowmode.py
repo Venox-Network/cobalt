1dd7f45a37a470db4d5fea491615890ef79300c0
DWAA1660 <matnip12345@gmail.com>
2022-05-18 14:56:45 -0600

    added remove slowmode and cleaned up code

--- cogs/dynamicslowmode.py
+++ cogs/dynamicslowmode.py
@@ -19,7 +19,7 @@ class dynamicslowmode(commands.Cog):
 
     @application_checks.has_permissions(manage_messages=True)
     @slash_command(
-        description="With a member it checks the last defined amount of messages and purges any that was sent by user")
+        description="Sets a slowmode that chnages depednign on how many messages were sent in the last minute")
     async def slowmodesetup(self, interaction: Interaction, amount_of_messages_per_min: int,
                             dynamic_max_slowmode_time: int, defaultslowmode: int,  channel: GuildChannel = SlashOption(channel_types=[ChannelType.text])):
         data = {"_id": channel.id, "channel_id": channel.id, "amount_of_messages_per_min": amount_of_messages_per_min,
@@ -31,39 +31,48 @@ class dynamicslowmode(commands.Cog):
             await dynamicslowmodedb.find_one_and_replace({"_id": channel.id}, data),
             await interaction.send("Dynamic slow-mode updated")
 
+    @application_checks.has_permissions(manage_messages=True)
+    @slash_command(
+        description="Removes dynamic slowmode")
+    async def removeslowmode(self, interaction: Interaction, channel: GuildChannel = SlashOption(channel_types=[ChannelType.text])):
+        data = {"_id": channel.id}
+        try:
+            await dynamicslowmodedb.delete_one(data)
+            await interaction.send(f"Slowmode removed for {channel.name}")
+        except Exception as e:
+            await interaction.send(f"Could not remove `{e}`")
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
         # db calls
-        resultsfind = await dynamicslowmodedb.find_one({"channel_id": message.channel.id})
-        amount_of_messages_per_min = resultsfind["amount_of_messages_per_min"]
-        slowmode_time = resultsfind["slowmode_time"]
-        channel_id_db = resultsfind["channel_id"]
-        defaultslowmode = resultsfind["defaultslowmode"]
-
+        try:
+            resultsfind = await dynamicslowmodedb.find_one({"channel_id": message.channel.id})
+            amount_of_messages_per_min = resultsfind["amount_of_messages_per_min"]
+            slowmode_time = resultsfind["slowmode_time"]
+            channel_id_db = resultsfind["channel_id"]
+            defaultslowmode = resultsfind["defaultslowmode"]
+        except:
+            return
         # checks the amount of messages per minute
 
         # gets the current time
         my_datetime = datetime.datetime.now()
         my_datetime_sec = my_datetime - datetime.timedelta(seconds=60)
         messages = await message.channel.history(limit=100, after=my_datetime_sec).flatten()
-        print(len(messages))
         if message.channel.id == channel_id_db:
             # checks if more messages was sent than the dynamic speed is
             if len(messages) >= amount_of_messages_per_min:
-                print("more messages than optimal")
                 # checks if the slowmode is already set
                 if message.channel.slowmode_delay != slowmode_time:
                     # sets the slowmode
                     await message.channel.edit(slowmode_delay=slowmode_time)
-                    print("throttled up")
 
             else:
                 if message.channel.slowmode_delay != defaultslowmode:
                     # sets the slowmode
                     await message.channel.edit(slowmode_delay=defaultslowmode)
-                    print("throttled down")
 
 
 def setup(client):
