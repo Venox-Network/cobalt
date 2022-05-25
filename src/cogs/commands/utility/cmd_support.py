e78d93e6c7d75c9ef615f585eac38f9b97a7fd27
DWAA1660 <matnip12345@gmail.com>
2022-05-25 15:19:09 -0600

    updated gitignore and stuff

--- src/cogs/commands/utility/cmd_support.py
+++ src/cogs/commands/utility/cmd_support.py
@@ -3,12 +3,12 @@ from discord import ApplicationContext
 from cogs import BaseCog
 
 
-def cog_creator(_servers: List[int]):
+def cog_creator(servers: List[int]):
     class Support(BaseCog):
 
         @BaseCog.cslash_command(
             description="Sends link to support server",
-            guild_ids=_servers
+            guild_ids=servers
         )
         async def support(self, ctx: ApplicationContext):
             await ctx.respond("[Join our support server](https://discord.gg/kaddCVeRj6)\nhttps://discord.gg/kaddCVeRj6")
