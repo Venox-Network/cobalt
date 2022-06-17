cbaf61c8209d39b68170edecaef1f905ad82c765
DWAA1660 <matnip12345@gmail.com>
2022-06-17 16:11:29 -0600

    fixed pinging in thread

--- src/cogs/commands/utility/cmd_auto_thread.py
+++ src/cogs/commands/utility/cmd_auto_thread.py
@@ -57,7 +57,7 @@ def cog_creator(servers: List[int]):
                 if message.channel.id not in self.thread_guild_map.values():
                     return
 
-                await message.create_thread(name=message.content[:15] + ("..." if len(message.content) > 15 else ""))
+                await message.create_thread(name=message.clean_content[:15] + ("..." if len(message.content) > 15 else ""))
             except Exception:
                 pass
 
