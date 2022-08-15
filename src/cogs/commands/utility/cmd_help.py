d610b22ad21c513031abb8d426290964e8881625
DWAA1660 <matnip12345@gmail.com>
2022-08-15 15:46:50 -0600

    fixed error added timestamp

--- src/cogs/commands/utility/cmd_help.py
+++ src/cogs/commands/utility/cmd_help.py
@@ -1,7 +1,6 @@
 from this import s
 from typing import List
 from discord import ApplicationContext
-from cobalt import return_commands
 import discord
 import datetime
 from discord.ext.commands import Cog
@@ -21,14 +20,15 @@ def cog_creator(servers: List[int]):
             em = discord.Embed(
                 title='Commands',
                 description='Shows all commands',
-                colour=0x2c6dbf
+                colour=0x2c6dbf,
+                timestamp=datetime.datetime.utcnow()
                 )
             commands = ctx.bot.walk_application_commands()
             for command in commands:
                 description = getattr(command, 'description')
                 em.add_field(name = command, value=description)
             now = datetime.datetime.now()
-            em.set_footer(text=f"Venox Network {now.month}/{now.day}/{now.year} {now.hour}:{now.minute}", icon_url='https://images-ext-1.discordapp.net/external/Ku-I5AZNKBTGjAQ2TRpUsTUiUKPVa7GKyidFvaQikik/%3Fsize%3D1024/https/cdn.discordapp.com/icons/879734848946847774/40e90339ddce11f97924e85c6e901b0c.png?width=491&height=491')
+            em.set_footer(text="Venox Network", icon_url='https://images-ext-1.discordapp.net/external/Ku-I5AZNKBTGjAQ2TRpUsTUiUKPVa7GKyidFvaQikik/%3Fsize%3D1024/https/cdn.discordapp.com/icons/879734848946847774/40e90339ddce11f97924e85c6e901b0c.png?width=491&height=491')
             await ctx.respond(embed=em)
     return HelpCog
     
