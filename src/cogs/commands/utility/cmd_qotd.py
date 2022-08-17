361647b0a54e87c8019fe000f4c5a6e837ab5ac5
DWAA1660 <matnip12345@gmail.com>
2022-08-17 11:29:16 -0600

    Update cmd_qotd.py

--- src/cogs/commands/utility/cmd_qotd.py
+++ src/cogs/commands/utility/cmd_qotd.py
@@ -64,10 +64,10 @@ def cog_creator(servers: List[int]):
                         for guild in self.bot.guilds:
                             for channel in guild.channels:
                                 if channel.name == 'qotd':
-                                    time.sleep(1)
                                     message = await channel.send(res['question'])
                                     message = self.bot.get_message(message.id)
                                     await message.create_thread(name=f"QOTD {now.month}-{now.day}-{now.year}")
+                                    time.sleep(1)
                     return
                 self.qotd_db.update_one({ 'id' : res['id'] },{ '$set': { 'used' : True } })
                 for guild in self.bot.guilds:
