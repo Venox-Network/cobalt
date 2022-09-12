f303ea51746be72a13ef133874ea04c9565312ee
DWAA1660 <matnip12345@gmail.com>
2022-09-12 17:21:50 -0600

    fixed indents

--- src/cogs/commands/utility/cmd_qotd.py
+++ src/cogs/commands/utility/cmd_qotd.py
@@ -39,9 +39,9 @@ def cog_creator(servers: List[int]):
             qotd_list = qotds.split(seperator)
             for qotd in qotd_list:
             	async for doc in documents:
-                if doc['id'] >= id:
-                    id = doc['id']
-                await self.qotd_db.insert_one({'id': int(id) + 1, 'question': qotd, 'used': False, 'user': str(ctx.user)})
+                    if doc['id'] >= id:
+                        id = doc['id']
+            await self.qotd_db.insert_one({'id': int(id) + 1, 'question': qotd, 'used': False, 'user': str(ctx.user)})
 
 	    
         @qotd.command(
