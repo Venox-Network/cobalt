9cc60991e9879be7283ec436a35f07885936a78e
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-13 01:43:31 -0400

    Update dependencies and bump version (2.0.0.)
    Gradle: 8.14 -> 8.14.3
    Shadow: 8.3.6 -> 8.3.8
    Lazy Library: 3.2.0 -> 976f3062f3
    JDA: 5.3.2 -> 5.6.1
    LibeTranslate: 1.0.5 -> 1.0.9
    LavaPlayer: 1.4.2 -> 1.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
 import xyz.srnyx.gradlegalaxy.utility.magicMongo
 import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
@@ -7,19 +6,18 @@ import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 plugins {
     application
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
 repositories.mavenLocal()
 
 magicMongo("ef0c2370bd")
-setupLazyLibrary("3.2.0", "5.3.2", "network.venox", "1.2.0", "A Discord bot for Venox Network")
+setupLazyLibrary("976f3062f3", "5.6.1", "network.venox", "2.0.0", "A Discord bot for Venox Network")
 
-repository(Repository.CLOJARS)
-
+repository("https://maven.dynomake.space/releases/") // space.dynomake:libretranslate-java
 dependencies {
-    implementation("net.clojars.suuft", "libretranslate-java", "1.0.5") // Translate
-    implementation("com.github.walkyst", "lavaplayer-fork", "1.4.2") // TTS
+    implementation("space.dynomake", "libretranslate-java", "1.0.9") // Translate
+    implementation("com.github.walkyst", "lavaplayer-fork", "1.4.3") // TTS
     implementation("net.sf.sociaal", "freetts", "1.2.2") // TTS
 
     compileOnly("io.github.freya022", "BotCommands", "2.10.4") // For documentation
