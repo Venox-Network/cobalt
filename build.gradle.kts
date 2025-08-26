816be547440f43b8819420b6d448acb1d9847849
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-26 14:48:05 -0400

    Update Gradle Galaxy
    1.3.3 -> 1.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,17 @@
 import xyz.srnyx.gradlegalaxy.enums.repository
-import xyz.srnyx.gradlegalaxy.utility.lazyLibrary
 import xyz.srnyx.gradlegalaxy.utility.magicMongo
-import xyz.srnyx.gradlegalaxy.utility.setupJda
+import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 
 
 plugins {
     application
-    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.4"
     id("com.gradleup.shadow") version "8.3.8"
     id("dev.reformator.stacktracedecoroutinator") version "2.5.6"
 }
 
 magicMongo("ef0c2370bd")
-lazyLibrary("botcommands-v3-SNAPSHOT")
-setupJda("5.6.1", "network.venox", "2.0.0", "A Discord bot for Venox Network")
+setupLazyLibrary("botcommands-v3-SNAPSHOT", "5.6.1", "network.venox", "2.0.0", "A Discord bot for Venox Network")
 
 repository("https://maven.dynomake.it/releases/") // space.dynomake:libretranslate-java
 dependencies {
@@ -21,12 +19,5 @@ dependencies {
     implementation("com.github.walkyst", "lavaplayer-fork", "1.4.3") // TTS
     implementation("net.sf.sociaal", "freetts", "1.2.2") // TTS
 
-    compileOnly("org.spongepowered:configurate-yaml:4.1.2") // For documentation
     compileOnly("org.mongodb", "mongodb-driver-sync", "5.2.0") // For documentation
 }
-
-// Fix some tasks
-tasks["distZip"].dependsOn("shadowJar")
-tasks["distTar"].dependsOn("shadowJar")
-tasks["startScripts"].dependsOn("shadowJar")
-tasks["startShadowScripts"].dependsOn("jar")
