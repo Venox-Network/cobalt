496372f81b07ddc4a69b6e5f31685c2977df41d3
srnyx <25808801+srnyx@users.noreply.github.com>
2025-09-14 20:35:56 -0400

    Update Gradle Galaxy
    1.3.4 -> 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 
 plugins {
     application
-    id("xyz.srnyx.gradle-galaxy") version "1.3.4"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.5"
     id("com.gradleup.shadow") version "8.3.8"
     id("dev.reformator.stacktracedecoroutinator") version "2.5.6"
 }
