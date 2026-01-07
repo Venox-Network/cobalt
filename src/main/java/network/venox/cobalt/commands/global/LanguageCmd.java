d62d698aa9692c313da9036def642e64927bbf67
srnyx <25808801+srnyx@users.noreply.github.com>
2026-01-07 01:51:41 -0500

    Improve code for `/language`

--- src/main/java/network/venox/cobalt/commands/global/LanguageCmd.java
+++ src/main/java/network/venox/cobalt/commands/global/LanguageCmd.java
@@ -5,6 +5,7 @@ import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.Updates;
 
 import io.github.freya022.botcommands.api.commands.CommandPath;
+import io.github.freya022.botcommands.api.commands.annotations.Command;
 import io.github.freya022.botcommands.api.commands.application.ApplicationCommand;
 import io.github.freya022.botcommands.api.commands.application.CommandScope;
 import io.github.freya022.botcommands.api.commands.application.slash.GlobalSlashEvent;
@@ -13,7 +14,7 @@ import io.github.freya022.botcommands.api.commands.application.slash.annotations
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.TopLevelSlashCommandData;
 
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.Command.Choice;
 
 import network.venox.cobalt.MongoProvider;
 import network.venox.cobalt.mongo.CoUser;
@@ -21,23 +22,25 @@ import network.venox.cobalt.mongo.CoUser;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import space.dynomake.libretranslate.Language;
+
 import xyz.srnyx.lazylibrary.LazyEmoji;
 
 import java.util.List;
 
 
-@io.github.freya022.botcommands.api.commands.annotations.Command
-public class Language extends ApplicationCommand {
-    @NotNull private static final List<Command.Choice> LANGUAGE_CHOICES = Translate.LANGUAGES.stream()
+@Command
+public class LanguageCmd extends ApplicationCommand {
+    @NotNull private static final List<Choice> LANGUAGE_CHOICES = Translate.LANGUAGES.stream()
             .map(language -> {
                 final String name = language.name();
-                return new Command.Choice(name.charAt(0) + name.substring(1).toLowerCase(), name);
+                return new Choice(name.charAt(0) + name.substring(1).toLowerCase(), name);
             })
             .toList();
 
     @NotNull private final MongoProvider mongo;
 
-    public Language(@NotNull MongoProvider mongo) {
+    public LanguageCmd(@NotNull MongoProvider mongo) {
         this.mongo = mongo;
     }
 
@@ -48,31 +51,28 @@ public class Language extends ApplicationCommand {
     public void language(@NotNull GlobalSlashEvent event,
                          @SlashOption(description = "Your new primary language") @NotNull String language) {
         // Get language
-        final space.dynomake.libretranslate.Language languageEnum;
+        final Language languageEnum;
         try {
-            languageEnum = space.dynomake.libretranslate.Language.valueOf(language.toUpperCase());
+            languageEnum = Language.valueOf(language.toUpperCase());
         } catch (final IllegalArgumentException e) {
             event.reply(LazyEmoji.NO + " Invalid language: `" + language + "`!").setEphemeral(true).queue();
             return;
         }
 
-        // Update
-        final CoUser previousUser = mongo.database.getMagicCollection(CoUser.class).findOneAndUpdate(
+        // Upsert
+        final CoUser previousUser = mongo.database.getMagicCollection(CoUser.class).findOneAndUpdate( // Can't use findOneAndUpsert because it returns AFTER document (we need BEFORE)
                 Filters.eq("_id", event.getUser().getIdLong()),
                 Updates.set("language", languageEnum),
                 new FindOneAndUpdateOptions().upsert(true));
 
         // Reply
         final StringBuilder reply = new StringBuilder(LazyEmoji.YES + " Your primary language is now `" + languageEnum + "`");
-        if (previousUser != null) {
-            final space.dynomake.libretranslate.Language previous = previousUser.language;
-            if (previous != null) reply.append(" (previous: `").append(previous).append("`)");
-        }
+        if (previousUser != null && previousUser.language != null) reply.append(" (previous: `").append(previousUser.language).append("`)");
         event.reply(reply.toString()).setEphemeral(true).queue();
     }
 
     @Override @NotNull
-    public List<Command.Choice> getOptionChoices(@Nullable Guild guild, @NotNull CommandPath commandPath, @NotNull String optionName) {
+    public List<Choice> getOptionChoices(@Nullable Guild guild, @NotNull CommandPath commandPath, @NotNull String optionName) {
         return LANGUAGE_CHOICES;
     }
 }
