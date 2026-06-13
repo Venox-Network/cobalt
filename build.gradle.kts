b6f21ccf254b0ce9cb673c91a5f103f6df3af876
srnyx <25808801+srnyx@users.noreply.github.com>
2026-06-13 00:29:59 -0400

    UPDATE LAZY LIBRARY... AGAIN...

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ setupLazyLibrary(
         version = "2.0.0",
         description = "A Discord bot for Venox Network"),
     jdaConfig = DependencyConfig(version = "6.4.2"),
-    lazyLibraryConfig = DependencyConfig(version = "e969338"))
+    lazyLibraryConfig = DependencyConfig(version = "e924489"))
 magicMongo(config = DependencyConfig(version = "3eff81b"))
 
 repository(Repository.JITPACK)
